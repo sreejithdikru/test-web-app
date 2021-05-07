9ca20f1e76b6763fd931865f0c4f5213bcc2964d
sreejith <imsreejithofficial@gmail.com>
2021-05-07 16:42:01 +0530

    this is a veg shop 1st commit

--- learn/urls.py
+++ learn/urls.py
@@ -21,6 +21,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('',include('webpage.urls')),
+    path('',include('vegshop.urls')),
     path('admin/', admin.site.urls),
 ]
